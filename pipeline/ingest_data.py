2f84de83ad0c56ba9db2be7da4a4a40b6bce0d09
AA <a.akosile@gmail.com>
2026-01-24 23:16:43 +0000

    Added docker compose and also updated iter loop text.

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -7,6 +7,7 @@ from sys import prefix
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 
 
@@ -35,20 +36,17 @@ parse_dates = [
 ]
 
 
-def run():
-
-    year = 2021
-    month = 1
-    
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    
-    chunksize = 100000
-
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--year', default=2021, type=int, help='Year of data to ingest')
+@click.option('--month', default=1, type=int, help='Month of data to ingest')
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-password', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name in the database')
+def run(year, month, pg_user, pg_password, pg_host, pg_port, pg_db, chunksize, target_table):
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
@@ -68,8 +66,10 @@ def run():
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists = 'replace')
+            print("Inserted first chunk. Rows:", len(df_chunk))
             first = False
-        df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists = 'append')
+        df_chunk.to_sql(name=target_table, con=engine, if_exists = 'append')
+        print("Inserted rows:", len(df_chunk))
 
 if __name__ == '__main__':
     run()
